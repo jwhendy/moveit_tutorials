a0654616daba3e3cd54926476b63250c66e28986
John Henderson <jw.hendy@gmail.com>
2017-08-03 16:19:41 -0500

    abridged tutorial example of cartesian path

---
+++ doc/pr2_tutorials/planning/scripts/move_group_python_interface_tutorial_abridged.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+
+from std_msgs.msg import String
+
+def move_group_python_interface_tutorial():
+
+  print "============ Starting tutorial setup"
+  moveit_commander.roscpp_initialize(sys.argv)
+  rospy.init_node('move_group_python_interface_tutorial',
+                  anonymous=True)
+
+  robot = moveit_commander.RobotCommander()
+  scene = moveit_commander.PlanningSceneInterface()
+  group = moveit_commander.MoveGroupCommander("left_arm")
+
+  display_trajectory_publisher = rospy.Publisher(
+                                      '/move_group/display_planned_path',
+                                      moveit_msgs.msg.DisplayTrajectory,
+                                      queue_size=20)
+
+  print "============ Waiting for RVIZ..."
+  rospy.sleep(2)
+
+  print '========== state before cartesian: %s' % group.get_current_pose()
+  
+  waypoints = []
+
+  # start with the current pose
+  waypoints.append(group.get_current_pose().pose)
+
+  # first orient gripper and move forward (+x)
+  wpose = geometry_msgs.msg.Pose()
+  wpose.orientation.w = 1.0
+  wpose.position.x = waypoints[0].position.x + 0.1
+  wpose.position.y = waypoints[0].position.y
+  wpose.position.z = waypoints[0].position.z
+  waypoints.append(copy.deepcopy(wpose))
+
+  # second move down
+  wpose.position.z -= 0.10
+  waypoints.append(copy.deepcopy(wpose))
+
+  # third move to the side
+  wpose.position.y += 0.05
+  waypoints.append(copy.deepcopy(wpose))
+
+  ## We want the cartesian path to be interpolated at a resolution of 1 cm
+  ## which is why we will specify 0.01 as the eef_step in cartesian
+  ## translation.  We will specify the jump threshold as 0.0, effectively
+  ## disabling it.
+  (plan3, fraction) = group.compute_cartesian_path(
+                               waypoints,   # waypoints to follow
+                               0.01,        # eef_step
+                               0.0)         # jump_threshold
+                               
+  print "============ Waiting while RVIZ displays plan3..."
+  rospy.sleep(5)
+
+  # Uncomment the line below to execute this plan on a real robot.
+  # I'm doing it because in the previous examples of only using plan()
+  # RViz always references the default robot state for the move and
+  # it isn't updated. I wanted to find out what the end position really was
+  group.execute(plan3)
+  rospy.sleep(10)
+  
+  print '========== state after cartesian: %s' % group.get_current_pose()
+  
+  ## When finished shut down moveit_commander.
+  moveit_commander.roscpp_shutdown()
+
+  ## END_TUTORIAL
+
+  print "============ STOPPING"
+
+
+if __name__=='__main__':
+  try:
+    move_group_python_interface_tutorial()
+  except rospy.ROSInterruptException:
+    pass
+
